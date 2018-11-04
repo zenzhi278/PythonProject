4125b8d5398c8a81810aef27ca9aecf3ea298642
zenzhi278 <zenzhi278@qq.com>
2018-11-04 22:21:06 +0800

    add prettyCharacterCount.py

---
+++ prettyCharacterCount.py
@@ -0,0 +1,9 @@
+# prettyCharacterCount.py
+import pprint
+message='It was a birght cold day in April, and the clocks were striking thirteen.'
+count={}
+for character in message:
+    count.setdefault(character,0)
+    count[character]=count[character]+1
+# pprint.pprint(count)
+print(pprint.pformat(count))
