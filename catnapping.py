aad367b1d0d3df60aad36098b9fa5686ea2f011f
zenzhi278 <zenzhi278@qq.com>
2018-11-04 23:21:16 +0800

    add catnapping.py

---
+++ catnapping.py
@@ -0,0 +1,26 @@
+print('''Dear Alice,
+
+Eve's cat has been arrested
+for catnapping, cat burglary
+and extortion.
+
+Sincerely,
+Bob'''
+)
+
+print('Dear Alice,\n\nEve\'s cat has been arrested for catnapping,cat burglary, and extortion.\n\nSincerely,\nBob')
+
+"""
+This is a test Python program.
+Written by AI Sweigart al@inventwithpthon.com
+
+This program was designed for python 3, not python 2.
+"""
+def spam():
+    """
+    This is a multiline comment to help 
+        explain what the spam() function does.
+    """
+    print('Hello!')
+
+spam()
